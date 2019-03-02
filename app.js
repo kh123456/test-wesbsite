adaabfe23d5ed29cb7695af51373f565232356af
khollenbeck <kris.hollenbeck@infor.com>
2019-03-01 21:09:32 -0600

    adding reset

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+ '/index.html'));
 });
